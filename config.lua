10121189439199b7338eeb934431b7fc147c663e
adminbot1 <j4vicl@gmail.com>
2017-03-01 17:25:14 +0330

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-bot_token = ""
+bot_token = "326706679:AAHB7cc4H3zpGyo51dLZNqqeJQlb7dl1B_w"
 send_api = "https://api.telegram.org/bot"..bot_token
 bot_version = "2.5"
-sudo_id = 212400472
+sudo_id = 296844543
